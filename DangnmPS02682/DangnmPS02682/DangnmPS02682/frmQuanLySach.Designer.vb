6359edc56c3af6de14347d1031f1dd5da9048100
Dangnmps02682 <dangnmps02682@fpt.edu.vn>
2015-07-02 11:15:31 +0700

    Dangnm
    lan 1

---
+++ DangnmPS02682/DangnmPS02682/DangnmPS02682/frmQuanLySach.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmQuanLySach
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvQuanLySach = New System.Windows.Forms.DataGridView()
+        Me.txtMaSach = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtTenSach = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtDonGia = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtMaLoai = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.dtpNgayXuatBan = New System.Windows.Forms.DateTimePicker()
+        Me.btnThem = New System.Windows.Forms.Button()
+        Me.btnSua = New System.Windows.Forms.Button()
+        CType(Me.dgvQuanLySach, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvQuanLySach
+        '
+        Me.dgvQuanLySach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvQuanLySach.Location = New System.Drawing.Point(331, 12)
+        Me.dgvQuanLySach.Name = "dgvQuanLySach"
+        Me.dgvQuanLySach.Size = New System.Drawing.Size(557, 406)
+        Me.dgvQuanLySach.TabIndex = 0
+        '
+        'txtMaSach
+        '
+        Me.txtMaSach.Font = New System.Drawing.Font("Arial Narrow", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtMaSach.Location = New System.Drawing.Point(143, 12)
+        Me.txtMaSach.Name = "txtMaSach"
+        Me.txtMaSach.Size = New System.Drawing.Size(182, 32)
+        Me.txtMaSach.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Narrow", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 25)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Mã sách"
+        '
+        'txtTenSach
+        '
+        Me.txtTenSach.Font = New System.Drawing.Font("Arial Narrow", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTenSach.Location = New System.Drawing.Point(143, 59)
+        Me.txtTenSach.Name = "txtTenSach"
+        Me.txtTenSach.Size = New System.Drawing.Size(182, 32)
+        Me.txtTenSach.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial Narrow", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 25)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Tên sách"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial Narrow", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 109)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(130, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Ngày xuất bản"
+        '
+        'txtDonGia
+        '
+        Me.txtDonGia.Font = New System.Drawing.Font("Arial Narrow", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDonGia.Location = New System.Drawing.Point(143, 153)
+        Me.txtDonGia.Name = "txtDonGia"
+        Me.txtDonGia.Size = New System.Drawing.Size(182, 32)
+        Me.txtDonGia.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial Narrow", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 156)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(75, 25)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Đơn giá"
+        '
+        'txtMaLoai
+        '
+        Me.txtMaLoai.Font = New System.Drawing.Font("Arial Narrow", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtMaLoai.Location = New System.Drawing.Point(143, 200)
+        Me.txtMaLoai.Name = "txtMaLoai"
+        Me.txtMaLoai.Size = New System.Drawing.Size(182, 32)
+        Me.txtMaLoai.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial Narrow", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(12, 203)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(72, 25)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Mã loại"
+        '
+        'dtpNgayXuatBan
+        '
+        Me.dtpNgayXuatBan.CustomFormat = "dd/mm/yyyy"
+        Me.dtpNgayXuatBan.Font = New System.Drawing.Font("Arial Narrow", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpNgayXuatBan.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpNgayXuatBan.Location = New System.Drawing.Point(143, 106)
+        Me.dtpNgayXuatBan.Name = "dtpNgayXuatBan"
+        Me.dtpNgayXuatBan.Size = New System.Drawing.Size(182, 32)
+        Me.dtpNgayXuatBan.TabIndex = 3
+        '
+        'btnThem
+        '
+        Me.btnThem.AutoSize = True
+        Me.btnThem.Font = New System.Drawing.Font("Arial Narrow", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnThem.Location = New System.Drawing.Point(32, 275)
+        Me.btnThem.Name = "btnThem"
+        Me.btnThem.Size = New System.Drawing.Size(110, 35)
+        Me.btnThem.TabIndex = 6
+        Me.btnThem.Text = "Thêm"
+        Me.btnThem.UseVisualStyleBackColor = True
+        '
+        'btnSua
+        '
+        Me.btnSua.AutoSize = True
+        Me.btnSua.Font = New System.Drawing.Font("Arial Narrow", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSua.Location = New System.Drawing.Point(195, 275)
+        Me.btnSua.Name = "btnSua"
+        Me.btnSua.Size = New System.Drawing.Size(110, 35)
+        Me.btnSua.TabIndex = 7
+        Me.btnSua.Text = "Sửa"
+        Me.btnSua.UseVisualStyleBackColor = True
+        '
+        'frmQuanLySach
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(899, 430)
+        Me.Controls.Add(Me.btnSua)
+        Me.Controls.Add(Me.btnThem)
+        Me.Controls.Add(Me.dtpNgayXuatBan)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtMaLoai)
+        Me.Controls.Add(Me.txtDonGia)
+        Me.Controls.Add(Me.txtTenSach)
+        Me.Controls.Add(Me.txtMaSach)
+        Me.Controls.Add(Me.dgvQuanLySach)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "frmQuanLySach"
+        Me.Text = "frmQuanLySach"
+        CType(Me.dgvQuanLySach, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents dgvQuanLySach As System.Windows.Forms.DataGridView
+    Friend WithEvents txtMaSach As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtTenSach As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtDonGia As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtMaLoai As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents dtpNgayXuatBan As System.Windows.Forms.DateTimePicker
+    Friend WithEvents btnThem As System.Windows.Forms.Button
+    Friend WithEvents btnSua As System.Windows.Forms.Button
+
+End Class
---
+++ DangnmPS02682/DangnmPS02682/DangnmPS02682/frmQuanLySach.vb
@@ -0,0 +1,88 @@
+﻿Imports System.Data.SqlClient
+
+Public Class frmQuanLySach
+    Dim chuoiketnoi As String = "Workstation id=testinf205.mssql.somee.com;packet size=4096;user id=TestINF205;pwd=Abcd@123;data source=testinf205.mssql.somee.com;persist security info=False;initial catalog=testinf205"
+    Dim database As New DataTable
+
+    Private Sub frmQuanLySach_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim ketnoi As SqlConnection = New SqlConnection(chuoiketnoi)
+        Dim lenh1 As SqlDataAdapter = New SqlDataAdapter("select * from Sach", ketnoi)
+
+        ketnoi.Open()
+        lenh1.Fill(database)
+        dgvQuanLySach.DataSource = database.DefaultView
+    End Sub
+
+    Private Sub btnThem_Click(sender As Object, e As EventArgs) Handles btnThem.Click
+        Dim ketnoi As SqlConnection = New SqlConnection(chuoiketnoi)
+        Dim lenh2 As String = "insert into Sach values (@MaSach,@TenSach,@NgayXuatBan,@DonGia,@MaLoai)"
+        Dim thucthi As New SqlCommand(lenh2, ketnoi)
+        ketnoi.Open()
+        Try
+            thucthi.Parameters.AddWithValue("@MaSach", txtMaSach.Text)
+            thucthi.Parameters.AddWithValue("@TenSach", txtTenSach.Text)
+            thucthi.Parameters.AddWithValue("@NgayXuatBan", dtpNgayXuatBan.Value)
+            thucthi.Parameters.AddWithValue("@DonGia", txtDonGia.Text)
+            thucthi.Parameters.AddWithValue("@MaLoai", txtMaLoai.Text)
+            MessageBox.Show("Thêm sách thành công")
+            ketnoi.Close()
+        Catch ex As Exception
+            MessageBox.Show("Không thể thêm sách")
+            ketnoi.Close()
+        End Try
+
+        Dim lammoi As SqlDataAdapter = New SqlDataAdapter("select * from Sach", ketnoi)
+
+        database.Clear()
+        lammoi.Fill(database)
+        dgvQuanLySach.DataSource = database.DefaultView
+
+    End Sub
+
+
+    Private Sub btnSua_Click(sender As Object, e As EventArgs) Handles btnSua.Click
+        Dim ketnoi As SqlConnection = New SqlConnection(chuoiketnoi)
+        Dim lenh3 As String = "Update Sach set MaSach = @MaSach,TenSach = @TenSach,NgayXuatBan = @NgayXuatBan,DonGia = @DonGia,LoaiSach_MaLoai = @MaLoai"
+        Dim thucthi As New SqlCommand(lenh3, ketnoi)
+        ketnoi.Open()
+        Try
+            thucthi.Parameters.AddWithValue("@MaSach", txtMaSach.Text)
+            thucthi.Parameters.AddWithValue("@TenSach", txtTenSach.Text)
+            thucthi.Parameters.AddWithValue("@NgayXuatBan", dtpNgayXuatBan.Value)
+            thucthi.Parameters.AddWithValue("@DonGia", txtDonGia.Text)
+            thucthi.Parameters.AddWithValue("@MaLoai", txtMaLoai.Text)
+            MessageBox.Show("Sửa sách thành công")
+            ketnoi.Close()
+        Catch ex As Exception
+            MessageBox.Show("Không thể sửa sách")
+            ketnoi.Close()
+        End Try
+
+        Dim lammoi As SqlDataAdapter = New SqlDataAdapter("select * from Sach", ketnoi)
+
+        database.Clear()
+        dgvQuanLySach.DataSource = database
+        dgvQuanLySach.DataSource = Nothing
+        loaddata()
+    End Sub
+
+    Private Sub dgvQuanLySach_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvQuanLySach.CellContentClick
+        Dim index As Integer = dgvQuanLySach.CurrentCell.RowIndex
+        txtMaSach.Text = dgvQuanLySach.Item(0, index).Value
+        txtTenSach.Text = dgvQuanLySach.Item(1, index).Value
+        dtpNgayXuatBan.Value = dgvQuanLySach.Item(2, index).Value
+        txtDonGia.Text = dgvQuanLySach.Item(3, index).Value
+        txtMaLoai.Text = dgvQuanLySach.Item(4, index).Value
+    End Sub
+
+    Private Sub loaddata()
+        Dim ketnoi As SqlConnection = New SqlConnection(chuoiketnoi)
+        Dim lenh4 As SqlDataAdapter = New SqlDataAdapter("select * from Sach", ketnoi)
+
+        ketnoi.Open()
+        lenh4.Fill(database)
+        dgvQuanLySach.DataSource = database.DefaultView
+
+    End Sub
+
+End Class
